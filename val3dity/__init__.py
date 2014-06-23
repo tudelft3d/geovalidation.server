87fe6ca0fac36e35e2497cf7ea0ea279286cfd25
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-06-23 10:48:00 +0200

    forgot that one

---
+++ val3dity/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from settings import *
+
+app = Flask(__name__, static_url_path='')
+
+app.config['UPLOAD_FOLDER']       = UPLOAD_FOLDER
+app.config['REPORTS_FOLDER']      = REPORTS_FOLDER
+app.config['PROBLEMFILES_FOLDER'] = PROBLEMFILES_FOLDER
+app.config['TMP_FOLDER']          = TMP_FOLDER
+
+
+
+from .views import *
