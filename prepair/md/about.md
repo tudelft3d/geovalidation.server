a17c5640ca0c66e85c9296a0b5d16ed14ee8fed4
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-08-18 11:57:11 +0200

    first complete beta working

--- prepair/md/about.md
+++ prepair/md/about.md
@@ -5,7 +5,9 @@
 
 prepair--pronounce 'pee-repair' as in 'polygon repair'--permits us to easily repair 'broken' GIS polygons according to the international standard [ISO19107](http://www.iso.org/iso/catalogue_detail.htm?csnumber=26012) (Geographic information--Spatial schema). Given one input polygon (or a MultiPolygon), it *automatically* repairs it and returns back a valid polygon (actually a MultiPolygon since the input can represent more than one polygon--think of a 'bowtie' for instance).
 
-It is an [open-source project](https://github.com/tudelft-gist/prepair) released under the GPL licence. We offer here a simplified version: only the [WKT](http://en.wikipedia.org/wiki/Well-known_text) of a polygon (or MultiPolygon) can be given as input, and it is repaired with an extension of the [odd-even algorithm](https://en.wikipedia.org/wiki/Even-odd_rule) to handle GIS polygons containing inner rings and degeneracies. For other options, download the [code](https://github.com/tudelft-gist/prepair) and compile it. 
+It is an [open-source project](https://github.com/tudelft-gist/prepair) released under the GPL licence. We offer here a simplified version: only the [WKT](http://en.wikipedia.org/wiki/Well-known_text) of a polygon (or MultiPolygon) can be given as input, and it is repaired with an extension of the [odd-even algorithm](https://en.wikipedia.org/wiki/Even-odd_rule) to handle GIS polygons containing inner rings and degeneracies. Also, only rather small WKT are supported.
+
+For repairing bigger polygons in *shapefiles* for instance, or for more repairing options, download the [code](https://github.com/tudelft-gist/prepair) and compile it. 
 
 Most of the details of the implementation are available in this scientific article:
 
--- prepair/views.py
+++ prepair/views.py
@@ -5,29 +5,12 @@ from flask import render_template, request, redirect, url_for
 import os
 import subprocess
 
+MAXSIZE = 1e4
 
 
 @app.route('/about')
 def about():
     return render_template("about.html")
-
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-    
-@app.route('/faq')
-def faq():
-    return render_template("faq.html")
-
-
-# def get_job_id():
-#     return str(uuid.uuid4()).split('-')[0]
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     return render_template("index.html")
-
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -36,20 +19,26 @@ def index():
     if request.method == 'POST':
         thewkt = request.form['thewkt']
         if thewkt == "":
-            return render_template("index.html", problem="You must give a polygon's WKT")
-        print thewkt
+            return render_template("index.html", problem="Error: no WKT given.")
+        if len(thewkt) > MAXSIZE:
+            return render_template("index.html", problem="Error: WKT too big (max allowed is %d char)." % MAXSIZE)
         cmd = []
         cmd.append(PREPAIREXE)
         cmd.append("--wkt")
         cmd.append(r"%s" % thewkt)
-        # cmd.append(r"POLYGON((0 0, 0 10, 10 0, 10 10, 0 0))")
         op = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         R = op.poll()
         if R:
           res = op.communicate()
           raise ValueError(res[1])
-        o =  op.communicate()[0]
-        return render_template("index.html", success=o)
+        # o = op.communicate()[0]
+        o = op.communicate()
+        if (o[0] != ""):
+            return render_template("index.html", success = o[0])
+        else:
+            if o[1] == "":
+                return render_template("index.html", problem="Error: something went wrong. Check the input WKT.")
+            return render_template("index.html", problem=o[1])
     return render_template("index.html")
 
 
