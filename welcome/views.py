8a31b55ed6cf6cc111f122e0bd4348e920d503c8
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-12-03 11:31:09 +0100

    now welcome page at the root
    it was about time, I know...

--- welcome/views.py
+++ welcome/views.py
@@ -5,3 +5,7 @@ from flask import render_template, send_from_directory
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
