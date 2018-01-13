20795110d7221164976a8642e1789de12f76b299
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-01-13 17:23:39 +0100

    version of val3dity shown online

--- val3dity/views.py
+++ val3dity/views.py
@@ -146,6 +146,16 @@ def query_db(query, args=(), one=False):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    os.chdir(app.config['VAL3DITYEXE_FOLDER'])
+    cmd = []
+    cmd.append("./val3dity")
+    cmd.append("--version")
+    op = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    R = op.poll()
+    if R:
+      res = op.communicate()
+      raise ValueError(res[1])
+    val3dityversion = op.communicate()[0].split()[2]
     if request.method == 'POST':
         f = request.files['file']
         if f:
@@ -184,7 +194,7 @@ def index():
               return render_template("index.html", problem='Uploaded file is not a valid file.')
         else:
             return render_template("index.html", problem='No file selected.')
-    return render_template("index.html")
+    return render_template("index.html", version=val3dityversion)
 
 
 @app.route('/reports/<jobid>')
