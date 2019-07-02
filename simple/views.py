6679716287f9dcab60ff237f72f25f23c1049dc7
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-07-02 13:48:42 +0200

    Fix views for report in html w/o the sqlite

--- simple/views.py
+++ simple/views.py
@@ -2,7 +2,6 @@ from simple import app
 
 import runvalidation
 
-from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory, _app_ctx_stack
 from werkzeug.utils import secure_filename
 import os
@@ -11,7 +10,7 @@ import uuid
 import time
 import json
 import copy
-from geoip import geolite2
+
 
 TRUSTED_PROXIES = {'127.0.0.1'}  
 
@@ -74,7 +73,6 @@ def index():
                 ignore204 = 'ignore204' in request.form
                 geom_is_sem_surfaces = 'geom_is_sem_surfaces' in request.form
                 uploadtime = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime())
-
                 jid = str(uuid.uuid4())
                 summary = runvalidation.validate(
                     jid, 
@@ -88,23 +86,7 @@ def index():
                     app.config['VAL3DITYEXE_FOLDER'],    
                     app.config['REPORTS_FOLDER'])    
                 print summary
-                #-- write summary to database
-                # print summary
-                db = sqlite3.connect(app.config['DATABASE'])
-                db.row_factory = sqlite3.Row
-                db.execute('update tasks set validated=?, total_primitives=?, invalid_primitives=?, total_cityobjects=?, invalid_cityobjects=?, errors=? where jid=?',
-                           [
-                           1,
-                           summary['total_primitives'], 
-                           summary['invalid_primitives'], 
-                           summary['total_features'], 
-                           summary['invalid_features'], 
-                           summary['errors'], 
-                           jid])
-                db.commit()
-                db.close()
                 os.remove(app.config['UPLOAD_FOLDER']+fname)
-           
                 return redirect('/simple/reports/%s' % jid)
             else:
                 return render_template("index.html", problem='Uploaded file is not a valid file.', version=val3dityversion)
@@ -113,70 +95,26 @@ def index():
     return render_template("index.html", version=val3dityversion)
 
 
-@app.route('/stats')
-def stats():
-    totaljobs   = query_db('select count(*) from tasks', [], one=True)
-    totalsolids = query_db('select sum(total_primitives) from tasks', [], one=True)
-    totalinvalidsolids = query_db('select sum(invalid_primitives) from tasks', [], one=True)
-    percentage = int((float(totalinvalidsolids[0]) / float(totalsolids[0])) * 100)
-    last5 = query_db('select * from tasks order by timestamp desc limit 5', [], one=False)
-    a = []
-    for each in last5:
-      ipcountry = 'unknown'
-      if (each['ip'] is not None): 
-        ipobj = geolite2.lookup(each['ip'])
-        if ipobj is not None:
-          ipcountry = ipobj.country
-      a.append((each['timestamp'].replace("T", " "), each['total_primitives'], each['invalid_primitives'], each['errors'], ipcountry))
-    allerrors = query_db('select errors from tasks where errors is not null and errors!=-1', [])
-    myerr = {}
-    for es in allerrors:
-      tmp = es[0].split("-")
-      ei = map(int, tmp)
-      for e in ei:
-        if e not in myerr:
-          myerr[e] = 1
-        else:
-          myerr[e] += 1
-    higherr = 100;
-    highest = 0;
-    for e in myerr:
-      if myerr[e] > highest:
-        highest = myerr[e]
-        higherr = e
-    return render_template("stats.html", 
-                           totaljobs="{:,}".format(totaljobs[0]),
-                           totalsolids="{:,}".format(totalsolids[0]),
-                           totalinvalidsolids="{:,}".format(totalinvalidsolids[0]),
-                           percentageinvalids=percentage,
-                           mosterror=higherr,
-                           last5=a
-                          )
 
 @app.route('/reports/<jobid>')
 def reports(jobid):
-    #-- check if job is in the database
-    db = query_db('select * from tasks where jid = ?', [jobid], one=True)
-    if db is None:
+    #-- check if file exists
+    if (os.path.isfile(app.config['REPORTS_FOLDER'] + jobid + ".json") == False):
         return render_template("status.html", notask=True, info="Error: this report number doesn't exist.", refresh=False)
-    #-- it exists
-    fname = db['file']
-    # print fname
-    if (db['validated'] == 0):
-        celtask = celery.AsyncResult(jobid)
-        if (celtask.ready() == False):
-            # print "task not finished."
-            return render_template("status.html", notask=False, info='Validation in progress: %s' % fname, refresh=True)
-    success = True
-    if (db['errors'] != "-1"):
-      success = False
+    j = json.loads(open(app.config['REPORTS_FOLDER'] + jobid + ".json").read())
+    fname = j["input_file"]
+    fname = fname[fname.rfind('/')+1:]
+    if j["invalid_primitives"] == 0 and j["invalid_features"] == 0:
+        success = True
+    else:
+        success = False
     return render_template("report.html", 
                            filename=fname, 
                            jid=jobid, 
-                           total_primitives=db["total_primitives"], 
-                           invalid_primitives=db["invalid_primitives"], 
-                           total_features=db["total_cityobjects"], 
-                           invalid_features=db["invalid_cityobjects"], 
+                           total_primitives=j["total_primitives"], 
+                           invalid_primitives=j["invalid_primitives"], 
+                           total_features=j["total_features"], 
+                           invalid_features=j["invalid_features"], 
                            welldone=success) 
     
 
@@ -217,26 +155,3 @@ def reports_primitives(jobid):
     j = json.loads(open(app.config['REPORTS_FOLDER'] + jobid + ".json").read())
     return render_template("report_Primitives.html", thereport=j)   
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        sqlite_db.row_factory = sqlite3.Row
-        top.sqlite_db = sqlite_db
-    return top.sqlite_db
-
-@app.teardown_appcontext
-def close_db_connection(exception):
-    """Closes the database again at the end of the request."""
-    top = _app_ctx_stack.top
-    if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
