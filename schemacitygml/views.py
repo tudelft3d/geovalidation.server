a85e5237cc72e8adc21c79ee9254337189216539
Hugo Ledoux <h.ledoux@tudelft.nl>
2020-05-06 15:37:16 +0200

    Put all python code for schemavalidation inside the views.py

--- schemacitygml/views.py
+++ schemacitygml/views.py
@@ -3,14 +3,63 @@ from schemacitygml import app
 from flask import render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
+from lxml import etree
 
-from .valxsdcitygml import validateonefile
+
 
 ALLOWED_EXTENSIONS = set(['gml', 'xml'])
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def validateonefile(fIn, folder):
+    doc, xsd = getXML(fIn, folder)
+    if (doc == None) and (xsd == None):
+        return "Invalid CityGML document: not a CityGML document."
+    xmlschema = etree.XMLSchema(xsd)
+    valid = doc.xmlschema(xsd)
+    if valid == True:
+        return True
+    else:
+        try:
+            xmlschema.assert_(doc)
+        except AssertionError as e:
+            return str(e)
+
+
+def getXML(fIn, folder):
+    parser = etree.XMLParser(ns_clean=True)
+    try:
+        doc = etree.parse(fIn)    
+    except:
+        doc = None
+        xsd = None
+        return doc, xsd
+    root = doc.getroot()
+    citygmlversion = ""
+    for key in root.nsmap.keys():
+        if root.nsmap[key].find('www.opengis.net/citygml') != -1:
+            if (root.nsmap[key][-3:] == '0.4'):
+                citygmlversion = '0.4'
+                break
+            if (root.nsmap[key][-3:] == '1.0'):
+                citygmlversion = '1.0'
+                break
+            if (root.nsmap[key][-3:] == '2.0'):
+                citygmlversion = '2.0'
+                break
+    if citygmlversion == "":
+        return None, None
+    if citygmlversion == "0.4":
+        xsd = etree.parse(folder + "schemas/v0.4/CityGML.xsd")
+    elif citygmlversion == "1.0":
+        xsd = etree.parse(folder + "schemas/v1.0/CityGML.xsd")
+    else:
+        xsd = etree.parse(folder + "schemas/v2.0/CityGML.xsd")
+    return doc, xsd
+
+
 
 
 @app.route('/about')
@@ -26,7 +75,7 @@ def index():
             if allowed_file(f.filename):
                 fname = secure_filename(f.filename)
                 f.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
-                re = valxsdcitygml.validateonefile(app.config['UPLOAD_FOLDER']+fname, app.config['SCHEMAROOT'])
+                re = validateonefile(app.config['UPLOAD_FOLDER']+fname, app.config['SCHEMAROOT'])
                 os.remove(app.config['UPLOAD_FOLDER']+fname)
                 if re == True:
                     return render_template("index.html", valid=True, fname=fname)           
